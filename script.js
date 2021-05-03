4439aeed5b1c794b82ac80bb0841dffa6ac4f60b
Kevin <kevin.vu06@gmail.com>
2021-05-03 19:44:34 +1000

    updated

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //add click event to all relevant boxes and to the restart button
+//declare variables x , board status, is the game active and a display to show the currentgamestatus + winning conditions
+
 const boxButton = document.querySelectorAll('.box')
 for (let i=0; i < boxButton.length; i++){
   boxButton[i].addEventListener("click", playerClick)
 }
 document.querySelector('.restart').addEventListener('click', restartGame);
-
-//what are our winning conditions as an array
+let currentPlayer = "X"
+let boardStatus = ["", "", "", "", "", "", "", "", ""];
 const winningConditions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -17,35 +19,29 @@ const winningConditions = [
   [2, 4, 6]
 ];
 
-//declare variables x , board status, is the game active and a display to show the currentgamestatus
-let currentPlayer = "X"
-let boardStatus = ["", "", "", "", "", "", "", "", ""];
-
 //1- function for player clicking
 //Need to make sure that the box that has been clicked is blank 
 // Also need to create two variables to be passed into the updateBoard function and the checkResult function.
 function playerClick(clickedBoxEvent) {
   let clickedBox = clickedBoxEvent.target;
   let clickedBoxIndex = parseInt(clickedBox.getAttribute('data-box-number'));
+  //to Check if there is a blank spot so we dont re-write it
   if (boardStatus[clickedBoxIndex] !== "" ) {
       return;
   }
-  //now we send the clickedBox and ClickedBoxIndex to our functions to update the board + check if anyone has won
+  //update the board HTML + the board array (originally blank)
+  boardStatus[clickedBoxIndex] = currentPlayer;
+  clickedBox.innerHTML = currentPlayer;
+
+  //add color to our X and O
   if (currentPlayer === "X"){
     clickedBox.style.color = 'red';
   } else if (currentPlayer === "O"){
     clickedBox.style.color = 'blue';
   }
-  updateBoard(clickedBox, clickedBoxIndex);
+  
   checkResult();
- 
-}
-//2 -function to update our array and what is displayed in our html file
-//we update the board with an x/o and the blank array- boardStatus - with X/O
-
-function updateBoard(clickedBox, clickedBoxIndex) {
-  boardStatus[clickedBoxIndex] = currentPlayer;
-  clickedBox.innerHTML = currentPlayer;
+  
 }
 //3 - function to check if we won
 function checkResult() {
