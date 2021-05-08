b98a54bd36f3e895df24c14631b51c169d8a5275
Kevin <kevin.vu06@gmail.com>
2021-05-08 10:15:19 +1000

    v2 with ai

--- v2/script.js
+++ v2/script.js
@@ -6,8 +6,8 @@ for (let i=0; i < boxButton.length; i++){
   boxButton[i].addEventListener("click", playerClick)
 }
 document.querySelector('.restart').addEventListener('click', restartGame);
-let currentPlayer = "X"
-const computerPlayer = 'O'
+const currentPlayer = "X";
+const computerPlayer = "O";
 //board status is the status of the board but the user doesnt see this
 let boardStatus = ["", "", "", "", "", "", "", "", ""];
 const winningConditions = [
@@ -20,6 +20,8 @@ const winningConditions = [
   [0, 4, 8],
   [2, 4, 6]
 ];
+
+gameFinished = false;
 
 //Game Counters
 let gameCounterDisplay = document.querySelector('.gamesPlayed');
@@ -51,22 +53,15 @@ function playerClick(clickedBoxEvent) {
   //update the board HTML + the board array (originally blank)
   boardStatus[clickedBoxIndex] = currentPlayer;
   clickedBox.innerHTML = currentPlayer;
-
-  //add color to our X and O
-  if (currentPlayer === "X"){
-    clickedBox.style.color = 'red';
-  } else if (currentPlayer === "O"){
-    clickedBox.style.color = 'blue';
-  }
-
+  clickedBox.classList.add("xChoice");
   checkResult();
-  
 }
 //3 - function to check if we won
 function checkResult() {
-  let roundWon = false;
-  for (let i = 0; i <= 7; i++) {
-      const winCondition = winningConditions[i];
+  if(gameFinished === false){
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+      let winCondition = winningConditions[i];
       //e.g for i = 0, winCondition = [0, 1, 2]
       let a = boardStatus[winCondition[0]];
       // e.g a = boardStatus[0]
@@ -81,42 +76,73 @@ function checkResult() {
       }
       if (a === b && b === c) {
           roundWon = true;
+          if (a === "X"){
+            xWinCounter ++;
+            gamesXWon.innerText = xWinCounter;
+          } 
+          document.querySelector('.whichWinner').innerText = 'Player '+ a + ' has won!!';
+          gameFinished = true
           break
       }
-  }
-  //Display Winning Message and also increase counter
-  if (roundWon == true) {
-    document.querySelector('.whichWinner').innerText = currentPlayer + ' has won!!';
-    if (currentPlayer == "X"){
-      xWinCounter ++;
-      gamesXWon.innerText = xWinCounter;
-    } else if (currentPlayer == "O"){
-      oWinCounter ++;
-      gamesOwon.innerText = oWinCounter;
     }
-    
-    return;
   }
+
   //Need an if condition to check if the board has been filled with no winner then we can display a draw message. gameDraw variable will be boolean and come back true or false. If the board has been filled but no winner has been determined it will be true. With true we can use another if statement to display a draw message
   let gameDraw = !boardStatus.includes("")
   if (gameDraw === true){
     drawCounter ++
     gamesDrawn.innerText = drawCounter;
-    document.querySelector('.whichWinner').innerText = 'Draw! No Winner'
+    document.querySelector('.whichWinner').innerText = 'Draw! No Winner';
+    gameFinished = true;
   }
-  //This will change the player marker from X to O
-  if (currentPlayer == 'X'){
-    currentPlayer = "O" ;
-  } else if (currentPlayer == "O"){
-    currentPlayer = "X"
-  };
+setTimeout(computerPlays, 500)
 }
 
+//Create an ai competitor that picks a random number from 0-8 . Then use an if statement, if the box is already filled the computer wont fill that box and will pick another number. if the box is empty they will put an O in that number.
 
+function computerPlays() {
+  if (gameFinished === false) {
+    var randomIndex = Math.floor(Math.random() * 9); // random number between 0-8
+    // rememeber : const boxButton = document.querySelectorAll('.box')
+    // checking to see if it has anything already filled
+    while (boardStatus[randomIndex] !== "" ) {
+        randomIndex = Math.floor(Math.random() * 9);
+      }
+
+    boardStatus[randomIndex] = computerPlayer;
+    boxButton[randomIndex].innerHTML = computerPlayer;
+
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        let winCondition = winningConditions[i];
+        //e.g for i = 0, winCondition = [0, 1, 2]
+        let a = boardStatus[winCondition[0]];
+        // e.g a = boardStatus[0]
+        let b = boardStatus[winCondition[1]];
+        //eg b = boardStatus[1]
+        let c = boardStatus[winCondition[2]];
+        // eg c = boardStatus[2]
+        //next we check if a=b=c as if they all have 'x' they will equal.
+        //first statement checks if they have any blanks which means we keep continuing
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            if (a === "O") {
+              oWinCounter ++;
+              gamesOwon.innerText = oWinCounter;
+            }
+            document.querySelector('.whichWinner').innerText = 'Player '+ a + ' has won!!';
+            break
+        }
+    }
+  }
+}
 //function to restart the game//
 function restartGame() {
-  currentPlayer = "X";
   boardStatus = ["", "", "", "", "", "", "", "", ""];
+  gameFinished = false;
   let restartBoxButtons = document.querySelectorAll('.box');
   for (let i = 0 ; i < restartBoxButtons.length; i ++){
     restartBoxButtons[i].innerHTML = "";
@@ -125,17 +151,3 @@ function restartGame() {
   gameCounter ++
   gameCounterDisplay.innerText = gameCounter;
 }
-/*
-//Create an ai competitor that picks a random number from 0-8 . Then use an if statement, if the box is already filled the computer wont fill that box and will pick another number. if the box is empty they will put an O in that number.
-
-var randomIndex = Math.floor(Math.random() * 9) // random number between 0-8
-var computerBox = boxButton[randomIndex]
-
-if (boardStatus[randomIndex] !== "" ) {
-    return;
-}// checking to see if it has anything already filled
-
-//update the board HTML + the board array (originally blank)
-boardStatus[randomIndex] = computerPlayer;
-computerBox.innerHTML = computerPlayer;
-*/
